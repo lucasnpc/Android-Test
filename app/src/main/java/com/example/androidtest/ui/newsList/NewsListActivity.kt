4f2579cdbfc6eb5b7ef3eef56e9c52c4bf061ddc
lucasnpc <lucasnpc2@outlook.com>
2021-03-31 16:25:20 -0300

    Adding adapter callback with lambda expressions

--- app/src/main/java/com/example/androidtest/ui/newsList/NewsListActivity.kt
+++ app/src/main/java/com/example/androidtest/ui/newsList/NewsListActivity.kt
@@ -1,26 +1,32 @@
 package com.example.androidtest.ui.newsList
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.example.androidtest.R
+import com.example.androidtest.data.model.News
 import com.example.androidtest.databinding.ActivityNewsListBinding
+import com.example.androidtest.ui.newsInfo.NewsInfoActivity
 import com.example.androidtest.ui.newsList.adapter.NewsListAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class NewsListActivity : AppCompatActivity() {
 
     private val newsListViewModel: NewsListViewModel by viewModel()
+    private lateinit var token: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = DataBindingUtil.setContentView<ActivityNewsListBinding>(this, R.layout.activity_news_list)
+        val binding = DataBindingUtil.setContentView<ActivityNewsListBinding>(
+            this,
+            R.layout.activity_news_list
+        )
 
-        val token = intent.getStringExtra("Token").toString()
+        token = intent.getStringExtra("Token").toString()
 
         binding.lifecycleOwner = this
         binding.viewModel = newsListViewModel
@@ -35,7 +41,10 @@ class NewsListActivity : AppCompatActivity() {
                 if (newsListResult.news != null) {
                     binding.newsList.apply {
                         visibility = View.VISIBLE
-                        adapter = NewsListAdapter(newsListResult.news, this@NewsListActivity, token)
+                        adapter = NewsListAdapter(
+                            newsList = newsListResult.news,
+                            context = this@NewsListActivity,
+                            cardItemClick = { news -> cardClick(news) })
                     }
                 }
             }
@@ -44,6 +53,18 @@ class NewsListActivity : AppCompatActivity() {
         binding.reload.setOnClickListener {
             newsListViewModel.getNews(token = token)
         }
+    }
+
+    private fun cardClick(news: News) {
+        updateUIWithNewsId(id = news.id_documento, token = token)
+    }
+
+    private fun updateUIWithNewsId(id: String, token: String) {
+        //TODO Update User Interface When the User Clicks on News
+        startActivity(
+            Intent(this, NewsInfoActivity::class.java).putExtra("id", id)
+                .putExtra("token", token)
+        )
     }
 
     override fun onBackPressed() {
--- app/src/main/java/com/example/androidtest/ui/newsList/adapter/NewsListAdapter.kt
+++ app/src/main/java/com/example/androidtest/ui/newsList/adapter/NewsListAdapter.kt
@@ -1,7 +1,6 @@
 package com.example.androidtest.ui.newsList.adapter
 
 import android.content.Context
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.annotation.NonNull
@@ -10,21 +9,30 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.androidtest.R
 import com.example.androidtest.data.model.News
 import com.example.androidtest.databinding.CardNewsListBinding
-import com.example.androidtest.ui.newsInfo.NewsInfoActivity
 import com.squareup.picasso.Picasso
 
 class NewsListAdapter(
     private val newsList: ArrayList<News>,
     private val context: Context,
-    private val token: String
+    private val cardItemClick: (News) -> Unit
 ) :
     RecyclerView.Adapter<NewsListAdapter.NewsViewHolder>() {
 
-    class NewsViewHolder(@NonNull val binding: CardNewsListBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class NewsViewHolder(@NonNull val binding: CardNewsListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun setCardClick(news: News) {
+            binding.cardNews.setOnClickListener { cardItemClick(news) }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         NewsViewHolder(
-            DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.card_news_list ,parent, false)
+            DataBindingUtil.inflate(
+                LayoutInflater.from(context),
+                R.layout.card_news_list,
+                parent,
+                false
+            )
         )
 
 
@@ -36,19 +44,9 @@ class NewsListAdapter(
         holder.binding.subtittleNews.text = news.linha_fina
         holder.binding.executePendingBindings()
 
-        holder.binding.cardNews.setOnClickListener {
-            updateUIWithNewsId(context, news.id_documento, token)
-        }
+        holder.setCardClick(news)
     }
 
     override fun getItemCount() = newsList.size
 
-    private fun updateUIWithNewsId(ctx: Context, id: String, token: String) {
-        //TODO Update User Interface When the User Clicks on News
-
-        ctx.startActivity(
-            Intent(context, NewsInfoActivity::class.java).putExtra("id", id)
-                .putExtra("token", token)
-        )
-    }
 }
